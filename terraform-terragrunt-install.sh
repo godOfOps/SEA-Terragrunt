aa7690fb4ab009d2fc799a1bcdc486bb0d3ae13a
Ankit Singh <ankitkrsingh13@gmail.com>
2024-05-01 17:53:17 +0530

    Adding Terrafrom, terragrunt install script

---
+++ terraform-terragrunt-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+: <<'SCRIPT_USAGE' #This is a multi-line comment
+
+curl https://raw.githubusercontent.com/godOfOps/SEA-Terragrunt/main/terraform-terragrunt-install.sh | bash
+
+SCRIPT_USAGE
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+terraform -install-autocomplete
+
+
+curl -L -o terragrunt https://github.com/gruntwork-io/terragrunt/releases/latest/download/terragrunt_linux_amd64
+chmod +x terragrunt
+sudo mv terragrunt /usr/local/bin
+terragrunt --install-autocomplete
+source ~/.bashrc
